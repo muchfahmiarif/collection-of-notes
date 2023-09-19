492e0326a0e1a9ef78130c5385c6059327577496
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-09-19 13:11:18 +0700

    `library` add data on database services

--- components/cards/Database.Cards.tsx
+++ components/cards/Database.Cards.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Data } from "@/public/data/databaseService";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import facebookImages from "@/public/images/facebook.svg";
 import Image from "next/image";
 
 const DatabaseCards = () => {
@@ -22,19 +21,19 @@ const DatabaseCards = () => {
         {Data.map((item) => (
           <Link href={item.url} key={item.id} target="_blank" className="">
             <div
-              className="group border border-white/10 px-20 h-14 rounded-lg flex flex-wrap hover:border-white/25 relative overflow-hidden justify-center items-center content-center"
+              className="group border border-white/10 px-20 h-14 rounded-lg flex flex-wrap hover:border-white/25 hover:bg-neutral-900 relative overflow-hidden justify-center items-center content-center duration-300"
               onMouseOver={() => handleHover(item.id)}
               onMouseLeave={handleMouseLeave}>
               {isHover === item.id && (
                 <Image
-                  src={facebookImages}
+                  src={item.image}
                   alt="Facebook Images"
                   className="shrink-0 min-h-full min-w-full items-center justify-center flex flex-wrap opacity-50"
                 />
               )}
 
               <div className="absolute flex flex-wrap left-0 pl-4 gap-4">
-                <p className={cn(`text-2xl`)}>{React.createElement(item.icon)}</p>
+                <p className={`text-2xl`}>{React.createElement(item.icon)}</p>
                 {item.name}
               </div>
             </div>
--- public/data/databaseService.ts
+++ public/data/databaseService.ts
@@ -1,5 +1,7 @@
-import { SiFauna, SiFirebase, SiMongodb, SiSupabase, SiPlanetscale } from "react-icons/si";
+import { SiFauna, SiFirebase, SiMongodb, SiSupabase, SiPlanetscale, SiUpstash, SiCockroachlabs, SiAmazondocumentdb } from "react-icons/si";
 import { HiDatabase } from "react-icons/hi";
+import { PiButterflyFill } from "react-icons/pi";
+import FacebookImage from "@/public/images/facebook.svg";
 
 export const Data = [
   {
@@ -7,36 +9,76 @@ export const Data = [
     name: "Mongo DB",
     url: "https://www.mongodb.com/",
     icon: SiMongodb,
-    iconColor: "text-blue-500",
+    image: FacebookImage,
   },
   {
     id: 2,
     name: "Fauna",
     url: "https://fauna.com/",
     icon: SiFauna,
+    image: FacebookImage,
   },
   {
     id: 3,
     name: "Firebase",
     url: "https://firebase.google.com/",
     icon: SiFirebase,
+    image: FacebookImage,
   },
   {
     id: 4,
     name: "Supabase",
     url: "https://supabase.io/",
     icon: SiSupabase,
+    image: FacebookImage,
   },
   {
     id: 5,
     name: "Planetscale",
     url: "https://planetscale.com/",
     icon: SiPlanetscale,
+    image: FacebookImage,
   },
   {
     id: 6,
     name: "Neon Tech",
     url: "https://neon.tech/",
     icon: HiDatabase,
+    image: FacebookImage,
+  },
+  {
+    id: 7,
+    name: "Upstash",
+    url: "https://upstash.com/",
+    icon: SiUpstash,
+    image: FacebookImage,
+  },
+  {
+    id: 8,
+    name: "Xata",
+    url: "https://xata.io/",
+    icon: PiButterflyFill,
+    image: FacebookImage,
+  },
+  {
+    id: 9,
+    name: "Cockroach DB",
+    url: "https://www.cockroachlabs.com/",
+    icon: SiCockroachlabs,
+    image: FacebookImage,
+  },
+  {
+    id: 10,
+    name: "Amazon Aurora",
+    url: "https://aws.amazon.com/aurora/",
+    icon: SiAmazondocumentdb,
+    image: FacebookImage,
+  },
+  {
+    id: 11,
+    name: "Amazon DynamoDB",
+    url: "https://aws.amazon.com/dynamodb/",
+    icon: SiAmazondocumentdb,
+    image: FacebookImage,
   },
 ];
