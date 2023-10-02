371180f302a8d2e1105769f4c94f10e93968abee
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-10-02 11:37:20 +0700

    🏡 add background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
-import Navbar from "@/components/navbar/menu-2.navbar";
-import { NavigationMenuDemo } from "@/components/navbar/menu.navbar";
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import { assets } from "@/utils/asset";
 import Image from "next/image";
+import { type Framework, frameworks } from "@/utils/frameworks";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
+  const [currentFramework, setCurrentFramework] = useState<Framework>(frameworks[0]);
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const rotateFrameworks = () => {
+      setCurrentFramework(frameworks[currentIndex]);
+      currentIndex = (currentIndex + 1) % frameworks.length;
+    };
+    const intervalId = setInterval(rotateFrameworks, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className={``}>
+      <div
+        className={cn("fixed inset-0 transition-colors delay-100 duration-700 opacity-25", {
+          "bg-purple-300": currentFramework === "qwik",
+          "bg-sky-300": currentFramework === "safari",
+          "bg-yellow-300": currentFramework === "chrome",
+          "bg-teal-300": currentFramework === "tailwind",
+          "bg-blue-300": currentFramework === "react",
+          "bg-green-300": currentFramework === "vue",
+          "bg-orange-400": currentFramework === "svelte",
+          "bg-red-300": currentFramework === "mobile",
+          "bg-neutral-300": currentFramework === "desktop",
+        })}></div>
       <Image
         src={assets.gradient}
         alt="gradient background"
---
+++ utils/frameworks.ts
@@ -0,0 +1,3 @@
+export const frameworks = ["react", "chrome", "qwik", "mobile", "tailwind", "desktop", "vue", "safari", "svelte"] as const;
+
+export type Framework = (typeof frameworks)[number];
