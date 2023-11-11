4b9f29a4bfa23ffe630fbd911e2915dcb0b1d09f
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-11-11 07:43:39 +0700

    add page on other

---
+++ pages/other.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Other = () => {
+  return (
+    <>
+      <h1>List extension chrome i use it</h1>
+      <ul>
+        <li>https://gofullpage.com/</li>
+        <li></li>
+      </ul>
+    </>
+  );
+};
+
+export default Other;
