5ea26d26be44c94ad4574e85ca50d920aabe1786
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-04-11 01:39:02 +0700

    add some data, create library maps and graphics

---
+++ data/(library)/(services)/maps.services.ts
@@ -0,0 +1,11 @@
+import { cardProps, defaultSomeType } from "@/types/card.type";
+
+export const dataMaps: cardProps[] = [
+  {
+    ...defaultSomeType,
+    id: 1,
+    name: "Leaflet",
+    url: "https://leafletjs.com/index.html",
+    tags: ["open source"],
+  },
+];
---
+++ data/(library)/(ui)/graphics.ui.ts
@@ -0,0 +1,11 @@
+import { cardProps, defaultSomeType } from "@/types/card.type";
+
+export const dataGraphics: cardProps[] = [
+  {
+    ...defaultSomeType,
+    id: 1,
+    name: "Mermaid",
+    url: "https://mermaid.js.org/",
+    tags: ["js", "free"],
+  },
+];
